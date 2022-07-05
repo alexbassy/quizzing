7155e3cf34277b3bceb873ab785c2c175c536a67
Alex Bass <alexpjbass@gmail.com>
2022-07-05 09:09:33 +0200

    Update to Vite v3 beta 🥸

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
