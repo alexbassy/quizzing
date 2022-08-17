33798fbcd2bc222116e9e33d65d4f2015b4796c1
Alex Bass <alexpjbass@gmail.com>
2022-08-17 09:27:54 +0200

    Working question navigation 🔁

--- src/pages/Play.vue
+++ src/pages/Play.vue
@@ -1,12 +1,11 @@
 <script lang="ts" setup>
-import { catchError, forkJoin, fromEvent, map, mergeMap, of, pluck, switchMap, tap } from 'rxjs'
-import { computed, ref, unref, watch } from 'vue'
+import { catchError, combineLatest, fromEvent, map, of, switchMap } from 'rxjs'
+import { computed, ref, watch } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import Slide from '@/components/slide'
 import { useSubscribe } from '@/composable/useObservable'
 import PlayLayout from '@/layouts/PlayLayout.vue'
 import { watch$ } from '@/lib/observables'
-import { getScoresFromCache, RoundState, saveScoresToCache, ScoresState } from '@/lib/scores'
 import { getQuestion$, getQuiz$, getRound$, getPlayersOfRound$ } from '@/lib/store/client'
 import { useObservable } from '@vueuse/rxjs'
 import PlayerAvatar from '@/components/player/PlayerAvatar.vue'
@@ -36,20 +35,16 @@ const quiz$ = round$.pipe(
 
 const players = useObservable(roundId$.pipe(switchMap(getPlayersOfRound$)))
 
-const quizId$ = quiz$.pipe(map((quiz) => quiz!.id!))
 const questionId$ = watch$(() => String(route.params.questionId))
-const quiz = useObservable(quizId$.pipe(switchMap((id) => getQuiz$(id))))
+const quiz = useObservable(quiz$)
 const prevNextQuestions = useObservable(
-  questionId$.pipe(
-    map((questionId) => {
-      const questions = quiz.value?.questions
+  combineLatest([quiz$, questionId$]).pipe(
+    map(([quiz, questionId]) => {
+      const questions = quiz!.questions
       if (!questions) return [null, null]
       const index = questions.indexOf(questionId)
       return [questions[index - 1], questions[index + 1]]
-    }),
-    mergeMap(([prevId, nextId]) =>
-      forkJoin([prevId ? getQuestion$(prevId) : of(null), nextId ? getQuestion$(nextId) : of(null)])
-    )
+    })
   ),
   { initialValue: [null, null] }
 )
@@ -66,7 +61,7 @@ const onPrevious = () => {
       return null
     }
     isPhotoShown.value = false
-    router.push({ params: { questionIndex: id.value - 1 } })
+    router.push({ params: { questionId: prevNextQuestions.value[0] } })
   }
 }
 
@@ -76,10 +71,11 @@ const onNext = () => {
     return
   }
   const nextQuestion = prevNextQuestions.value[1]
+  console.log(prevNextQuestions.value[1])
   if (!nextQuestion) {
     return null
   }
-  router.push({ params: { questionIndex: id.value + 1 } })
+  router.push({ params: { questionId: prevNextQuestions.value[1] } })
   isAnswerShown.value = false
   isPhotoShown.value = false
 }
