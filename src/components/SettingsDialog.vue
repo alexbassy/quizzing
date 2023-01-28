762e85f4db36c8fee4b4130e10bef9340e3db9ce
Alex Bass <alexpjbass@gmail.com>
2023-01-28 19:25:10 +0100

    Very basic and unusable tabs in settings dialog 🗂️

--- src/components/SettingsDialog.vue
+++ src/components/SettingsDialog.vue
@@ -20,29 +20,73 @@ async function doImport() {
     await importDatabase(file)
   }
 }
+
+const tabs = [
+  { id: 'importExport', label: 'Import/Export' },
+  { id: 'somethingElse', label: 'Something else' },
+] as const
+
+const activeTab = ref(tabs[0].id)
 </script>
 
 <template>
-  <Dialog v-slot="{ close }" v-bind="$attrs">
-    <h1 class="title">Settings</h1>
-    <div class="importExport">
-      <section>
-        <h2>Import</h2>
-        <input ref="fileInput" type="file" />
-        <SecondaryButton type="button" class="" @click="doImport">Import quizzes</SecondaryButton>
-
-        <h2>Export</h2>
-        <PrimaryButton type="button" @click="doExport">Export database</PrimaryButton>
-      </section>
+  <Dialog v-slot="{ close }" v-bind="$attrs" class="settings">
+    <div class="settings__tabsPane">
+      <h1 class="title">Settings</h1>
+      <ul class="settings__tabs">
+        <li v-for="tab in tabs" :key="tab.id" class="settings__tabItem">
+          <button
+            class="settings__tabItem"
+            :class="{ '-active': activeTab === tab.id }"
+            :aria-label="tab.label"
+            @click="activeTab = tab.id"
+          >
+            {{ tab.label }}
+          </button>
+        </li>
+      </ul>
     </div>
-    <div class="playDialogActions">
-      <SecondaryButton type="button" @click="close">Close</SecondaryButton>
+    <div class="settings__tabsContent">
+      <div v-if="activeTab === 'importExport'">
+        <div class="importExport">
+          <section>
+            <h2>Import</h2>
+            <input ref="fileInput" type="file" />
+            <SecondaryButton type="button" class="" @click="doImport">Import quizzes</SecondaryButton>
+
+            <h2>Export</h2>
+            <PrimaryButton type="button" @click="doExport">Export database</PrimaryButton>
+          </section>
+        </div>
+        <div class="playDialogActions">
+          <SecondaryButton type="button" @click="close">Close</SecondaryButton>
+        </div>
+      </div>
     </div>
   </Dialog>
 </template>
 
 <style lang="scss" scoped>
-.settingsDialog {
+.settings {
+  display: grid;
+  width: 600px;
+  max-width: 100vw;
+  height: 350px;
+  grid-template-columns: 2fr 4fr;
+
+  &__tabs {
+    display: flex;
+    flex-direction: column;
+    margin-top: 1rem;
+  }
+
+  &__tabItem {
+    padding: 0.25rem 0.5rem;
+    margin-left: -0.5rem;
+    &.-active {
+      background: rgb(255 255 255 / 15%);
+    }
+  }
 }
 
 .importExport {
