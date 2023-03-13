bc9faabc5f8d34e4dcb3f39ec638b21dd647daa1
Alex Bass <alexpjbass@gmail.com>
2023-03-13 08:51:22 +0100

    Write questions with AI 💖

--- src/worker/handler.ts
+++ src/worker/handler.ts
@@ -12,6 +12,7 @@ interface Bindings extends Context {
   bindings: {
     UNSPLASH_ACCESS_KEY: string
     TINIFY_API_KEY: string
+    OPENAI_API_KEY: string
   }
 }
 
@@ -89,6 +90,72 @@ API.add('POST', '/compress', async (req, context) => {
       'Content-Type': 'multipart/form-data',
     },
   })
+})
+
+API.add('POST', '/generate-question', async (req, context) => {
+  interface ChatGPTResponse {
+    choices: {
+      text: string
+    }[]
+  }
+
+  const prompt = await utils.body<string>(req)
+
+  if (!prompt) {
+    return reply(421, { message: 'No prompt' })
+  }
+
+  // Check if prompt is simple human language and not code
+  if (!/[^\W-]/g.test(prompt)) {
+    return reply(421, { message: 'Prompt should contain simple human language' })
+  }
+
+  async function requestChatGPT(topic: string): Promise<string> {
+    const url = 'https://api.openai.com/v1/completions'
+    const apiKey = context.bindings.OPENAI_API_KEY
+    const model = 'text-davinci-003'
+    const maxTokens = 300
+
+    const systemPrompt = `You are a making a quiz with your group of intelligent, fun loving friends. 8/10 difficulty. Questions and options should be fun and not overly complex. The quiz is multiple choice with four options. Only one correct answer and the other three options are incorrect. Vary the format and style of the questions to be interesting. Give a brief explanation or context for the correct answer in no more than 30 words. Provide the questions and the correct answer in minified JSON format (not pretty printed) as if it will be parsed directly. The only output should be the JSON.
+      The schema should be (TypeScript) \`{ title: string; options: string[]; correctAnswer: { index: number; explanation: string } }[]\` 
+      Start with three random questions. Topic: ${topic}`
+
+    const body = JSON.stringify({
+      prompt: systemPrompt,
+      max_tokens: maxTokens,
+      model: model,
+    })
+
+    const response = await fetch(url, {
+      method: 'POST',
+      body,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    })
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch ChatGPT response: ${response.statusText}`)
+    }
+
+    const json = await response.json<ChatGPTResponse>()
+    const completions = json.choices[0].text
+
+    console.log(json)
+
+    return completions
+  }
+
+  try {
+    const res = await requestChatGPT(prompt)
+    return reply(200, res)
+  } catch (e) {
+    console.log(e)
+    return reply(500, {
+      message: 'Questions could not be generated. The prompt might have broken the terms of use.',
+    })
+  }
 })
 
 // Attach "fetch" event handler
